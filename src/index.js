f414ee0c02d469ba6cce0c590459d89a4d067248
cadltop <158861368+cadltop@users.noreply.github.com>
2024-07-20 01:13:14 -0500

    Include about page on index and create tab switching.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,22 @@ import 'normalize.css';
 import './style.css';
 
 import {loadHome} from './homePage.js';
+import {loadAbout} from './aboutPage.js';
 
 loadHome();
+
+const container = document.querySelector('#container');
+function emptyContainer() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+document.querySelector('button#home').addEventListener('click', () => {
+    emptyContainer();
+    loadHome();
+});
+document.querySelector('button#about').addEventListener('click', () => {
+    emptyContainer();
+    loadAbout();
+});
