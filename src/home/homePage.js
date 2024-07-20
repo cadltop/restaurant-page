7ca838dc306e6bd1156c78818ed27421143019cc
cadltop <158861368+cadltop@users.noreply.github.com>
2024-07-20 15:50:45 -0500

    Add Schedule text on home page and style it.

--- src/home/homePage.js
+++ src/home/homePage.js
@@ -14,8 +14,18 @@ const loadHome = function() {
                 'and the conversation flows as easily as the wine we recommend. <br>' +
                 'The aromas of our freshly baked bread and slow-cooked sauces <br>mingle with ' +
                 'the laughter and chatter, creating an unforgettable <br>experience that leaves a lasting impression.';
+    
+    const scheduleTitle = document.createElement('h2');
+    scheduleTitle.innerHTML = 'Schedule';
+    
+    const scheduleInfo = document.createElement('p');
+    scheduleInfo.innerHTML = 'Monday to Saturday, 8AM - 9PM';
+
     textContainer.appendChild(headline);
     textContainer.appendChild(description);
+    textContainer.appendChild(scheduleTitle);
+    textContainer.appendChild(scheduleInfo);
+
     container.appendChild(textContainer);
 };
 
